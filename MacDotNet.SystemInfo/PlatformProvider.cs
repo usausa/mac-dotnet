c4c60020bc382b9fc8c46537c7b9df3f4132dd0a
usausa <machi.pon@gmail.com>
2026-01-04 10:12:56 +0900

    Update setting

--- MacDotNet.SystemInfo/PlatformProvider.cs
+++ MacDotNet.SystemInfo/PlatformProvider.cs
@@ -3,7 +3,6 @@ namespace MacDotNet.SystemInfo;
 using System.Runtime.Versioning;
 
 #pragma warning disable CA1024
-[SupportedOSPlatform("macos")]
 public static class PlatformProvider
 {
     public static UptimeInfo GetUptime() => new();
