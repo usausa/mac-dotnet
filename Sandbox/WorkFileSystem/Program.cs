51e66209212f434485b7e8f2c3036662975ace13
usausa <machi.pon@gmail.com>
2026-02-11 18:11:13 +0900

    Update

--- Sandbox/WorkFileSystem/Program.cs
+++ Sandbox/WorkFileSystem/Program.cs
@@ -4,6 +4,21 @@ internal static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        // TODO
     }
+
+    // TODO implement system info method
+}
+
+// ReSharper disable CollectionNeverQueried.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+#pragma warning disable IDE1006
+#pragma warning disable CA2101
+#pragma warning disable CA5392
+#pragma warning disable CS8981
+internal static class NativeMethods
+{
+    // TODO P/Invoke
 }
