636587533a7acd33bf961589f4581385df7c9706
usausa <machi.pon@gmail.com>
2026-03-08 21:39:53 +0900

    Update

--- MacDotNet.Disk/Assembly.cs
+++ MacDotNet.Disk/Assembly.cs
@@ -1,1 +1,7 @@
 [assembly: CLSCompliant(false)]
+
+// TODO delete
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "SA1611", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "SA1615", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "SA1623", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "SA1629", Justification = "Ignore")]
